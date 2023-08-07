150000be8cd38562352a5d61adb3388128f8ff40
Victoria Nikitina <victoria.nikitina@appliedmethods.biz>
2023-08-07 17:52:49 +0300

    slider mobile version

--- source/js/script.js
+++ source/js/script.js
@@ -5,6 +5,10 @@ const serviceFeaturesScreens = document.querySelectorAll('.service-slider__item'
 const classActiveFeaturesButton = 'active';
 const classActiveServiceSliderScreen = 'service-slider__item--active';
 
+const serviceFeaturesScreen = document.querySelector('.service-slider__item');
+
+serviceFeaturesScreen.classList.add('service-slider__item--active');
+
 const setActive = (element, classNameActive, classNameBase) => {
   document.querySelector(`.${classNameBase}.${classNameActive}`).classList.remove(classNameActive);
   element.classList.add(classNameActive)
@@ -18,7 +22,26 @@ serviceFeaturesButtonContainer.addEventListener('click', (e) => {
       'service-slider__item--active',
       'service-slider__item'
     );
+
+    //swiper.activeIndex
   }
 });
 
-const swiper = new Swiper('.swiper');
+const swiper = new Swiper('.swiper', {
+  slidesPerView: 1,
+  pagination: {
+    el: '.swiper__pagination',
+    slidesPerView: 1
+  },
+  // breakpoints: {
+  //   768: {
+  //     slidesPerView: 1
+  //   },
+  //   1000: {
+  //     slidesPerView: 5
+  //   },
+  //   1200: {
+  //     slidesPerView: null
+  //   }
+  // }
+});
